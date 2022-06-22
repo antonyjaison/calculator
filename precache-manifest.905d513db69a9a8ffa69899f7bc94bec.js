620f9ede89889848bade0e8a36306263342cea29
Antony Jaison <antonyjaison639@gmail.com>
2022-06-22 23:42:47 +0530

    Updates

---
+++ precache-manifest.905d513db69a9a8ffa69899f7bc94bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbb07b01e93f58d505e7",
+    "url": "/calculator/static/css/main.f2484e03.chunk.css"
+  },
+  {
+    "revision": "fbb07b01e93f58d505e7",
+    "url": "/calculator/static/js/main.fbb07b01.chunk.js"
+  },
+  {
+    "revision": "749c875ada2a1b38ca6c",
+    "url": "/calculator/static/js/1.749c875a.chunk.js"
+  },
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "81a8f193608f8473e552aaa333ce5b1c",
+    "url": "/calculator/index.html"
+  }
+];
